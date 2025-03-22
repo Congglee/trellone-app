7313f26362cf21e5f57ac040ecc8413995b8d178
Congglee <congldqn888@gmail.com>
2025-03-22 15:40:33 +0700

    feat: add functionality to move cards between columns with API integration

--- src/queries/boards.ts
+++ src/queries/boards.ts
@@ -1,7 +1,12 @@
 import { createApi } from '@reduxjs/toolkit/query/react'
 import { toast } from 'react-toastify'
 import axiosBaseQuery from '~/lib/redux/helpers'
-import { BoardResType, UpdateBoardBodyType } from '~/schemas/board.schema'
+import {
+  BoardResType,
+  MoveCardToDifferentColumnBodyType,
+  MoveCardToDifferentColumnResType,
+  UpdateBoardBodyType
+} from '~/schemas/board.schema'
 
 const BOARD_API_URL = '/boards' as const
 
@@ -13,13 +18,7 @@ export const boardApi = createApi({
   tagTypes,
   baseQuery: axiosBaseQuery(),
   endpoints: (build) => ({
-    updateBoard: build.mutation<
-      BoardResType,
-      {
-        id: string
-        body: UpdateBoardBodyType
-      }
-    >({
+    updateBoard: build.mutation<BoardResType, { id: string; body: UpdateBoardBodyType }>({
       query: ({ id, body }) => ({ url: `${BOARD_API_URL}/${id}`, method: 'PUT', data: body }),
       async onQueryStarted(_args, { queryFulfilled }) {
         try {
@@ -29,11 +28,23 @@ export const boardApi = createApi({
           console.error(error)
         }
       }
+    }),
+
+    moveCardToDifferentColumn: build.mutation<MoveCardToDifferentColumnResType, MoveCardToDifferentColumnBodyType>({
+      query: (body) => ({ url: `${BOARD_API_URL}/supports/moving-card`, method: 'PUT', data: body }),
+      async onQueryStarted(_args, { queryFulfilled }) {
+        try {
+          await queryFulfilled
+        } catch (error) {
+          toast.error('There was an error moving the card to a different column')
+          console.error(error)
+        }
+      }
     })
   })
 })
 
-export const { useUpdateBoardMutation } = boardApi
+export const { useUpdateBoardMutation, useMoveCardToDifferentColumnMutation } = boardApi
 
 const boardApiReducer = boardApi.reducer
 
