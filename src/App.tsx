5c71fb03122bdf63f1cc800f4cb8d06d15ba2595
Congglee <congldqn888@gmail.com>
2025-03-23 15:42:33 +0700

    feat: add 404 Not Found page with animated background and navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
-import { Route, Routes } from 'react-router-dom'
+import { Navigate, Route, Routes } from 'react-router-dom'
 import path from '~/constants/path'
+import NotFound from '~/pages/404/NotFound'
 import BoardDetails from '~/pages/Boards/BoardDetails'
 
 function App() {
   return (
     <Routes>
+      {/* Redirect Route */}
+      <Route path='/' element={<Navigate to='/boards/67dcfe028020577bb00a7048' replace={true} />} />
+
+      {/* Board Details */}
       <Route path={path.boardDetails} element={<BoardDetails />} />
+
+      {/* 404 not found page */}
+      <Route path='*' element={<NotFound />} />
     </Routes>
   )
 }
---
+++ src/pages/404/NotFound/index.ts
@@ -0,0 +1,3 @@
+import NotFound from '~/pages/404/NotFound/NotFound'
+
+export default NotFound
