ff30d392a6ed10207a0f69a87af3f1b74d41ee5f
Congglee <congldqn888@gmail.com>
2025-04-06 10:48:38 +0700

    feat: integrate email verification resend api

--- src/pages/Auth/AccountVerification/AccountVerification.tsx
+++ src/pages/Auth/AccountVerification/AccountVerification.tsx
@@ -1,15 +1,30 @@
 import { useEffect } from 'react'
 import { Navigate } from 'react-router-dom'
+import { toast } from 'react-toastify'
 import PageLoadingSpinner from '~/components/Loading/PageLoadingSpinner'
 import path from '~/constants/path'
 import useQueryConfig from '~/hooks/use-query-config'
-import { useVerifyEmailMutation } from '~/queries/auth'
+import { useAppSelector } from '~/lib/redux/hooks'
+import { useLogoutMutation, useVerifyEmailMutation } from '~/queries/auth'
 import { AuthQueryParams } from '~/types/query-params.type'
 
 export default function AccountVerification() {
   const { token, email } = useQueryConfig<AuthQueryParams>()
+  const { profile, isAuthenticated } = useAppSelector((state) => state.auth)
 
   const [verifyEmailMutation, { isLoading, isSuccess }] = useVerifyEmailMutation()
+  const [logoutMutation] = useLogoutMutation()
+
+  useEffect(() => {
+    if (isAuthenticated && profile) {
+      // If the user is authenticated and has a profile, we log them out to get a new access token
+      logoutMutation().then((res) => {
+        if (!res.error) {
+          toast.info('Please login again to enjoy our services')
+        }
+      })
+    }
+  }, [isAuthenticated, profile])
 
   useEffect(() => {
     if (token && email) {
