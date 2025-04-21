92137062807e908ac5edee84682e420ddacedbac
Congglee <congldqn888@gmail.com>
2025-04-21 19:44:12 +0700

    Update baseUrl to use environment-specific API URLs

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,5 +1,7 @@
+const environment = import.meta.env.MODE || 'development'
+
 export const envConfig = {
-  baseUrl: import.meta.env.VITE_APP_API_URL,
+  baseUrl: environment === 'development' ? import.meta.env.VITE_APP_DEV_API_URL : import.meta.env.VITE_APP_PROD_API_URL,
   googleClientId: import.meta.env.VITE_GOOGLE_CLIENT_ID,
   googleRedirectUri: import.meta.env.VITE_GOOGLE_REDIRECT_URI
 }
